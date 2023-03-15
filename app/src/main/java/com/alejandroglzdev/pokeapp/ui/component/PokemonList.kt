45360efe035cae753a95e6020e7166c7fd2dbd75
gonzalez.espejo.alejandro <gonzalez.espejo.alejandro@alumnat.copernic.cat>
2023-03-15 22:20:03 +0100

    Progress bar and constraint implemented.

--- app/src/main/java/com/alejandroglzdev/pokeapp/ui/component/PokemonList.kt
+++ app/src/main/java/com/alejandroglzdev/pokeapp/ui/component/PokemonList.kt
@@ -1,25 +1,40 @@
 package com.alejandroglzdev.pokeapp.ui.component
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.alejandroglzdev.pokeapp.data.model.PokemonModel
 
 @Composable
 fun PokemonList(pokemonList: List<PokemonModel>?) {
-    if (pokemonList != null) {
-        LazyColumn {
-            items(pokemonList) { pokemon ->
-                PokemonListView(pokemon)
+    ConstraintLayout(Modifier.fillMaxSize()) {
+        val circularProgressIndicator = createRef()
+        if (pokemonList != null) {
+            LazyColumn {
+                items(pokemonList) { pokemon ->
+                    PokemonListView(pokemon)
+                }
             }
+        }else{
+            CircularProgressIndicator(modifier = Modifier.constrainAs(circularProgressIndicator){
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            })
         }
     }
 
-}
-
-@Preview
-@Composable
-fun PreviewList() {
 
 }
+
