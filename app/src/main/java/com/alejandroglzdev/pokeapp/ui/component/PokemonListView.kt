2b749f07c44627eb6cdd2b20622ce57eadd972cc
gonzalez.espejo.alejandro <gonzalez.espejo.alejandro@alumnat.copernic.cat>
2023-08-16 12:51:17 +0200

    UI improved

--- app/src/main/java/com/alejandroglzdev/pokeapp/ui/component/PokemonListView.kt
+++ app/src/main/java/com/alejandroglzdev/pokeapp/ui/component/PokemonListView.kt
@@ -1,28 +1,137 @@
 package com.alejandroglzdev.pokeapp.ui.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.alejandroglzdev.pokeapp.R
 import com.alejandroglzdev.pokeapp.data.model.PokemonModel
 import com.alejandroglzdev.pokeapp.domain.model.Pokemon
 
 @Composable
 fun PokemonListView(pokemonModel: Pokemon) {
-    Column(
+    Surface(
+        shape = RoundedCornerShape(16.dp),
+        color = Color(0xFFDAE1E7),
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(24.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+            .height(210.dp)
+            .padding(10.dp),
+        elevation = 10.dp
     ) {
-        PokemonImage(image = pokemonModel.sprites.frontDefault,
-            10.dp)
-        PokemonListViewText(pokemonModel.pokedexNumber, textAlign = TextAlign.Center)
-        PokemonListViewText(pokemonModel.pokemonName, textAlign = TextAlign.Center)
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(2f),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(2f),
+                ) {
+                    Surface(
+                        shape = RoundedCornerShape(24.dp),
+                        modifier = Modifier.wrapContentSize(),
+                        color = Color(0xFFD1D5E1)
+                    ) {
+                        Text(
+                            text = "New release",
+                            fontSize =  12.sp,
+                            style = MaterialTheme.typography.h2,
+                            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+                        )
+                    }
+                    Surface(
+                        shape = RoundedCornerShape(24.dp),
+                        modifier = Modifier.wrapContentSize(),
+                        color = Color(0xFFD1D5E1)
+                    ) {
+                        Text(
+                            text = "New release",
+                            fontSize =  12.sp,
+                            style = MaterialTheme.typography.h2,
+                            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Text(
+                    text = pokemonModel.pokemonName,
+                    fontSize =  24.sp,
+                    style = MaterialTheme.typography.h1,
+                    fontWeight = FontWeight.SemiBold
+                )
+
+                Spacer(modifier = Modifier.height(2.dp))
+
+                Text(text = "#" + pokemonModel.pokedexNumber)
+
+                Spacer(modifier = Modifier.height(2.dp))
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    /*
+                    Text(
+                        text = "4.0",
+                        fontSize =  14.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        style = MaterialTheme.typography.h1
+                    )
+                    */
+
+                    Spacer(modifier = Modifier.width(4.dp))
+                }
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                OutlinedButton(
+                    shape = RoundedCornerShape(8.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = Color.Black,
+                        backgroundColor = Color.White
+                    ),
+                    onClick = { /*TODO*/ }
+                ) {
+                    Text(
+                        text = "Read More",
+                        fontSize =  11.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        style = MaterialTheme.typography.h2
+                    )
+                }
+            }
+
+            Surface(
+                shape = RoundedCornerShape(16.dp),
+                //El color este es duda si dejarlo o quitarlo
+                color = Color(0xFFDAE1E7),
+                modifier = Modifier.size(width = 130.dp, height = 170.dp)
+            ) {
+                PokemonImage(image = pokemonModel.sprites.frontDefault,
+                    20.dp)
+            }
+        }
     }
 
 }
@@ -30,6 +139,110 @@ fun PokemonListView(pokemonModel: Pokemon) {
 @Preview
 @Composable
 fun PreviewListView() {
-    //PokemonListView(PokemonProvider.getPokemonList()[1])
+    Surface(
+        shape = RoundedCornerShape(16.dp),
+        color = Color(0xFFDAE1E7),
+        modifier = Modifier
+            .height(210.dp)
+            .padding(10.dp),
+        elevation = 10.dp
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(2f),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(2f),
+                ) {
+                    Surface(
+                        shape = RoundedCornerShape(24.dp),
+                        modifier = Modifier.wrapContentSize(),
+                        color = Color(0xFFD1D5E1)
+                    ) {
+                        Text(
+                            text = "New release",
+                            fontSize =  12.sp,
+                            style = MaterialTheme.typography.h2,
+                            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+                        )
+                    }
+                    Surface(
+                        shape = RoundedCornerShape(24.dp),
+                        modifier = Modifier.wrapContentSize(),
+                        color = Color(0xFFD1D5E1)
+                    ) {
+                        Text(
+                            text = "New release",
+                            fontSize =  12.sp,
+                            style = MaterialTheme.typography.h2,
+                            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Text(
+                    text = "Gift Plant",
+                    fontSize =  24.sp,
+                    style = MaterialTheme.typography.h1,
+                    fontWeight = FontWeight.SemiBold
+                )
+
+                Spacer(modifier = Modifier.height(2.dp))
+
+                Text(text = "Price : 300$")
+
+                Spacer(modifier = Modifier.height(2.dp))
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        text = "4.0",
+                        fontSize =  14.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        style = MaterialTheme.typography.h1
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                }
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                OutlinedButton(
+                    shape = RoundedCornerShape(8.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = Color.Black,
+                        backgroundColor = Color.White
+                    ),
+                    onClick = { /*TODO*/ }
+                ) {
+                    Text(
+                        text = "Read More",
+                        fontSize =  11.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        style = MaterialTheme.typography.h2
+                    )
+                }
+            }
+
+            Surface(
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier.size(width = 100.dp, height = 140.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentScale = ContentScale.Crop,
+                    contentDescription = null
+                )
+            }
+        }
+    }
 }
 
