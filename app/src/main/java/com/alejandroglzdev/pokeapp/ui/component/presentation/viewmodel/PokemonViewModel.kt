cb997127d25631fcd2736bc299472ffe26914deb
gonzalez.espejo.alejandro <gonzalez.espejo.alejandro@alumnat.copernic.cat>
2023-08-16 12:27:41 +0200

    Pokemons name capitalized

--- app/src/main/java/com/alejandroglzdev/pokeapp/ui/component/presentation/viewmodel/PokemonViewModel.kt
+++ app/src/main/java/com/alejandroglzdev/pokeapp/ui/component/presentation/viewmodel/PokemonViewModel.kt
@@ -1,5 +1,6 @@
 package com.alejandroglzdev.pokeapp.ui.component.presentation.viewmodel
 
+import androidx.compose.ui.text.capitalize
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asFlow
@@ -14,15 +15,16 @@ import javax.inject.Inject
 @HiltViewModel
 class PokemonViewModel @Inject constructor(
     val getPokemonsUseCase: GetPokemonsUseCase
-    ) : ViewModel() {
+) : ViewModel() {
 
     val items = MutableLiveData<List<Pokemon>>()
     fun onCreate() {
         viewModelScope.launch {
             val result = getPokemonsUseCase(20, 1)
+            val pokemonsCapitalized = capitalize(result)
 
-            if (!result.isNullOrEmpty()) {
-                items.postValue(result)
+            if (!pokemonsCapitalized.isNullOrEmpty()) {
+                items.postValue(pokemonsCapitalized)
             }
         }
     }
@@ -30,16 +32,32 @@ class PokemonViewModel @Inject constructor(
     fun update(lastItemIndex: Int) {
         viewModelScope.launch {
             val result = getPokemonsUseCase(limit = lastItemIndex + 20, offSet = lastItemIndex)
+            val pokemonsCapitalized = capitalize(result)
 
-            if (!result.isNullOrEmpty()) {
+            if (!pokemonsCapitalized.isNullOrEmpty()) {
                 items.value?.let { currentList ->
-                    val updatedList = currentList + result
+                    val updatedList = currentList + pokemonsCapitalized
 
 
                     items.postValue(updatedList.distinctBy { it.pokedexNumber })
                 }
             }
         }
+    }
+
+    fun capitalize(pokemons: List<Pokemon>): List<Pokemon> {
+        var newList : MutableList<Pokemon> = mutableListOf()
+
+        for(pokemon in pokemons) {
+            var newPokemon = Pokemon(
+                pokemonName = pokemon.pokemonName.replaceFirstChar(Char::titlecase),
+                pokedexNumber = pokemon.pokedexNumber,
+                sprites = pokemon.sprites)
+
+            newList.add(newPokemon)
+        }
+
+        return newList
     }
 
 }
