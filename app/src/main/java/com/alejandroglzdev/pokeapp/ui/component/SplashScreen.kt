94de4b35c6ba07375e7631600796da7a7f4a4bc4
gonzalez.espejo.alejandro <gonzalez.espejo.alejandro@alumnat.copernic.cat>
2023-10-01 18:30:05 +0200

    Splash Screen done

---
+++ app/src/main/java/com/alejandroglzdev/pokeapp/ui/component/SplashScreen.kt
@@ -0,0 +1,53 @@
+package com.alejandroglzdev.pokeapp.ui.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import com.alejandroglzdev.pokeapp.R
+import com.alejandroglzdev.pokeapp.navigation.AppScreens
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(navController: NavHostController) {
+    LaunchedEffect(key1 = true){
+        delay(2000)
+        navController.popBackStack()
+        navController.navigate(AppScreens.HomeScreen.route)
+    }
+    Splash()
+}
+
+@Composable
+fun Splash() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_poke_ball),
+            contentDescription = "PokeBall",
+            Modifier.size(150.dp,150.dp))
+
+    }
+}
+
+@Preview
+@Composable
+fun SplashScreenPreview() {
+    Splash()
+}
+
+
